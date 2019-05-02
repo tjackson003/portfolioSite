75999acd49f9f461adc30b694dde7d10aa686b08
TJ Jackson <elitefourfresh@gmail.com>
2019-05-02 14:49:54 -0400

    carousel motion works

--- js/program.js
+++ js/program.js
@@ -1,3 +1,28 @@
-function topFunction() {
-    document.documentElement.scrollTop = 0;
+var carousel = document.querySelector('#workCaro');
+var figure = carousel.querySelector('figure');
+var leftNav = carousel.querySelector('.leftArrow');
+var rightNav = carousel.querySelector('.rightArrow');
+var numImages = figure.childElementCount;
+var theta = 2 * Math.PI / numImages;
+var currImage = 0;
+
+leftNav.addEventListener('click', onClick, true);
+rightNav.addEventListener('click', onClick, true);
+
+function onClick(e) {
+    e.stopPropagation();
+
+    var t = e.target;
+    if (t.tagName.toUpperCase() != 'IMG') {
+        return;
+    }
+    
+    if (t.classList.contains('next')) {
+        currImage++;
+    }
+    else {
+        currImage--;
+    }
+
+    figure.style.transform = `rotateY(${currImage * -theta}rad)`;
 }
